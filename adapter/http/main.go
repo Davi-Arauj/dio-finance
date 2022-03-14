0e64cd13e86442db2b2bfe626d38caf9a8afbf81
Davi-Arauj <moreiradaavi@gmail.com>
2022-03-14 11:39:06 -0300

    Usando metrics

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Davi-Arauj/dio-finance/adapter/http/actuator"
 	"github.com/Davi-Arauj/dio-finance/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 //Init bçalsjflakp
@@ -12,6 +13,7 @@ func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransaction)
 	http.HandleFunc("/transactions/create", transaction.CreateATransaction)
 	http.HandleFunc("/health", actuator.Health)
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":8080", nil)
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Davi-Arauj/dio-finance
 go 1.13
 
 require (
+	github.com/prometheus/client_golang v1.12.1
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/tools v0.1.9 // indirect
 )
